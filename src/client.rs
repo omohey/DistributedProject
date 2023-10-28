7cfc3c4aec23195e7c691f8cce75849dbf088b55
Omar Moheyeldin <omarmohey@aucegypt.edu>
2023-10-28 12:05:35 +0200

    multiple clients 1 server UDP

--- src/client.rs
+++ src/client.rs
@@ -1,69 +1,27 @@
 // client.rs
 
-use std::io::{Read, Write};
-use std::net::TcpStream;
-use std::thread;
-use std::time::{Duration, Instant};
+use std::net::UdpSocket;
+use std::io;
 
-fn send_request_to_server(number: i64, operation_flag: u8) -> i64 {
-    match TcpStream::connect("127.0.0.1:8080") {
-        Ok(mut stream) => {
-            // Send the request to the server (operation_flag + number)
-            let mut request = Vec::new();
-            request.push(operation_flag);
-            request.extend_from_slice(&number.to_be_bytes());
+fn main() -> io::Result<()> {
+    let socket = UdpSocket::bind("127.0.0.1:0")?; // Binding to 0 allows the OS to choose an available port
+    socket.connect("127.0.0.1:8080")?;
+    let mut increment = 0;
+    loop {
+        let input;
+        // println!("Type a message to send to the server:");
+        // io::stdin().read_line(&mut input)?;
 
-            if let Err(e) = stream.write_all(&request) {
-                eprintln!("Failed to write to server: {}", e);
-                return 0;
-            }
+        increment += 1;
+        input = increment.to_string();
+        socket.send(input.as_bytes())?;
 
-            let mut buffer = [0; 8];
-            // Read the result from the server
-            match stream.read_exact(&mut buffer) {
-                Ok(_) => {
-                    let result = i64::from_be_bytes(buffer);
-                    result
-                }
-                Err(e) => {
-                    eprintln!("Failed to read from server: {}", e);
-                    0
-                }
-            }
-        }
-        Err(e) => {
-            eprintln!("Failed to connect to server: {}", e);
-            0
-        }
+        let mut buffer = [0; 1024];
+        socket.recv_from(&mut buffer)?;
+
+        let response = String::from_utf8_lossy(&buffer);
+        println!("Received response: {}", response);
+        // make the thread sleep for 1 second
+        std::thread::sleep(std::time::Duration::from_secs(1));
     }
-}
-
-fn main() {
-    let number_of_requests = 100000;
-    let delay_duration = Duration::from_secs(1);
-
-    let mut total_duration = Duration::new(0, 0);
-
-    for i in 0..number_of_requests {
-        let iteration_start = Instant::now();
-
-        let number_to_increment = 102;
-        let result_increment = send_request_to_server(number_to_increment, 0); // 0 for increment
-        println!("Server responded with increment: {}", result_increment);
-
-        let number_to_decrement = 110;
-        let result_decrement = send_request_to_server(number_to_decrement, 1); // 1 for decrement
-        println!("Server responded with decrement: {}", result_decrement);
-
-        let iteration_time = iteration_start.elapsed();
-        total_duration += iteration_time;
-
-        println!("Iteration {} took: {:?}", i + 1, iteration_time);
-
-        // Add a delay between requests for better synchronization
-        // thread::sleep(delay_duration);
-    }
-    // Calculate and print the average duration
-    let average_duration = total_duration / number_of_requests as u32;
-    println!("Average iteration time: {:?}", average_duration);
 }
